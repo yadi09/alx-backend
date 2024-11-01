3f0cf1f4501b95c9de58d32835e386942528edd3
Yadamzer <yadamzerterefe09@gmail.com>
2024-11-01 03:39:23 +0300

    Task 1

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Create a class BasicCache that inherits from BaseCaching"""
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """BasicCache class"""
+    def put(self, key, item):
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        if key is None or key not in self.cache_data:
+            return None
+
+        return self.cache_data[key]
