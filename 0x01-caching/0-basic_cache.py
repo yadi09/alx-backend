9b73a37924e821a09a1ac628de9eb9b43e3c40ff
Yadamzer <yadamzerterefe09@gmail.com>
2024-11-01 03:44:15 +0300

    Task 1

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,17 +1,22 @@
 #!/usr/bin/env python3
-"""Create a class BasicCache that inherits from BaseCaching"""
+"""
+Create a class BasicCache that inherits from BaseCaching
+"""
 from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
     """
     BasicCache class that inherits from BaseCaching
+    Caching system
     """
     def put(self, key, item):
+        """ put = to add items """
         if key is not None and item is not None:
             self.cache_data[key] = item
 
     def get(self, key):
+        """ get = to retrive items """
         if key is None or key not in self.cache_data:
             return None
 
